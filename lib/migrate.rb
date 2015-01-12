cd8ed9f676a6f8749dcf7724d0b3a735e76deed0
Daniel Koch <koch@triple6.org>
2015-01-12 17:57:52 +0100

    cleanup

--- lib/migrate.rb
+++
@@ -1,7 +0,0 @@
-# Paste.where(hashid: nil).all.each do |paste|
-#   hashid = nil
-#   while hash.nil? || Paste.first(hashid: hashid)
-#     hashid = SecureRandom.urlsafe_base64(HASH_LENGTH)
-#   end
-#   paste.update(hashid: hashid)
-# end
