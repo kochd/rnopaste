917d6f013528209e715f32c673306bb959b0bd63
Daniel Koch <koch@triple6.org>
2015-02-04 22:18:51 +0100

    fixed broken expire

--- lib/expire.rb
+++ lib/expire.rb
@@ -2,7 +2,7 @@ Thread.new do
   Thread.current.priority = -3
   loop do
     sleep EXPIRES_CHECK_EVERY
-    Paste.all do |paste|
+    Paste.all.each do |paste|
       Paste.delete if paste.expires < Time.now
     end
   end
